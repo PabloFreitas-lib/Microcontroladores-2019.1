1bdd0267ce16c9d4eb203d09f6e7201d0e69078f
Pablo Freitas Santos <pablo_agua@hotmail.com>
2019-06-29 22:20:57 -0300

    Update P2_part3.c

--- P2_part3.c
+++ P2_part3.c
@@ -1,82 +1,100 @@
 #include "bib.h"
-#define GPIO_PF1_M1PWM5         0x00050405
-#define GPIO_PF2_M1PWM6         0x00050805
-#define GPIO_PF3_M1PWM7         0x00050C05
-/*
- *  PF1 as PWM outputs --> Timer0 B
- *  PF2 as PWM outputs --> Timer1 A
- *  PF3 as PWM outputs --> Timer1 B
- */
+
+
 void ADC0handler(void);
 void Systickhandler(void);
+void UART0Handler(void);
 
-//about 2ms at 80Mhz
-#define time 56666
-#define PWM_FREQ 55
-
-int i;
-int R[30] = {255,255,255,255,255,255,255,255,255,255,175 ,23,   1,  1,  1,  1,  1,1 ,1};
-int G[30] = {9  ,  5,  1,  1,  1,  1, 90, 50,130,220,255,255,255,255,200,100,130,50,1};
-int B[30] = {240,200,160,100, 40,  1,  1,  1,  1,  1,  1,  1, 50,100,255,255,255,255,255};
 
 int main(void)
 {
     SysCtlClockSet(SYSCTL_SYSDIV_2_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
     //Nokia
-   /* Nokia5110_Init();
+    Nokia5110_Init();
     Nokia5110_Clear();
-    Nokia5110_ClearBuffer();*/
+    Nokia5110_ClearBuffer();
 
     SysCtlPWMClockSet(SYSCTL_PWMDIV_1);// parao pwm dos leds
     SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);//para os leds
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);//para os leds
     SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);// para a temperatura
 
-    //ADC_config();
-    //verifica_temp();
+    //Uart
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 
+
+    IntMasterEnable(); // todas as interrupt
+    ADC_config();
+
+
+    // PWM
     GPIOPinConfigure(GPIO_PF1_M1PWM5);
     GPIOPinConfigure(GPIO_PF2_M1PWM6);
     GPIOPinConfigure(GPIO_PF3_M1PWM7);
 
-    GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+    //UART
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
 
+    GPIOPadConfigSet(GPIO_PORTA_BASE, GPIO_PIN_0|GPIO_PIN_1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD); // Uart
+
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0|GPIO_PIN_1); // UART
+    GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3); // PWM
+
+    //UART
+    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200, (UART_CONFIG_WLEN_8|UART_CONFIG_STOP_ONE|UART_CONFIG_PAR_NONE));
+
+    //PWM
     PWMGenConfigure(PWM1_BASE, PWM_GEN_2, PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC);
     PWMGenConfigure(PWM1_BASE, PWM_GEN_3, PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC);
 
+    //PWM
     PWMGenPeriodSet(PWM1_BASE, PWM_GEN_2, 320);
     PWMGenPeriodSet(PWM1_BASE, PWM_GEN_3, 320);
 
+    //PWM
     PWMPulseWidthSet(PWM1_BASE, PWM_OUT_5, 255);
     PWMPulseWidthSet(PWM1_BASE, PWM_OUT_6, 255);
     PWMPulseWidthSet(PWM1_BASE, PWM_OUT_7, 255);
 
+    //PWM
     PWMGenEnable(PWM1_BASE, PWM_GEN_2);
     PWMGenEnable(PWM1_BASE, PWM_GEN_3);
 
+    //PWM
     PWMOutputState(PWM1_BASE, PWM_OUT_5_BIT | PWM_OUT_6_BIT | PWM_OUT_7_BIT, true);
 
+    UARTEnable(UART0_BASE);
+    UARTIntEnable(UART0_BASE, UART_INT_RX|UART_INT_RT);
 
 
-    //IntMasterEnable(); // todas as interrupt
-    /*SysTickPeriodSet(0xFFFFFF);
-
-    SysTickEnable();
-    SysTickIntEnable();
+    IntEnable(INT_UART0);
     IntEnable(INT_ADC0SS0);
-    ADCIntEnable(ADC0_BASE, 0);*/
+    ADCIntEnable(ADC0_BASE, 0);
+
+
+
+    //UART
+    UARTFIFOLevelSet(UART0_BASE, UART_FIFO_TX1_8, UART_FIFO_RX4_8);
+    UARTFIFOEnable(UART0_BASE);
+    IntMasterEnable();
+
+    //SysTickIntEnable();
+    //SysTickEnable();
+    //SysTickPeriodSet(0xFFFFFF);
 
 
     while(1)
     {
-       for( i = 0; i<16; i++){
-           PWMPulseWidthSet(PWM1_BASE, PWM_OUT_5,R[i]);
-           PWMPulseWidthSet(PWM1_BASE, PWM_OUT_7,G[i]);
-           PWMPulseWidthSet(PWM1_BASE, PWM_OUT_6,B[i]);
-           SysCtlDelay(1000000);
-       }
+        if(uart_recebida == 1)
+        {
+            //IntMasterDisable();
+            flow();
+        }
+
         // temperatura
-        /*ADCProcessorTrigger(ADC0_BASE, 0);
+        ADCProcessorTrigger(ADC0_BASE, 0);
         // Passo 11
         ui32TempAvg = (ui32ADC0Value[0] + ui32ADC0Value[1] + ui32ADC0Value[2] + ui32ADC0Value[3]+ ui32ADC0Value[4] + ui32ADC0Value[5] + ui32ADC0Value[6] + ui32ADC0Value[7] + 4)/8;
 
@@ -84,9 +102,27 @@ int main(void)
         ui32TempValueC = (1475 - ((2475 * ui32TempAvg)) / 4096);
 
         //Tarefa 1 lab6
-        temperatura_calc(ui32TempValueC);*/
+        temperatura_calc(ui32TempValueC);
 
+        if(++ctd == 200)
+        {
+            verifica_temp();
+            ctd=0;
+        }
     }
+}
+
+void UART0Handler(void)
+{
+    UARTIntClear(UART0_BASE, UART_INT_RX|UART_INT_RT);
+    unsigned char aux_char;
+    aux_char = UARTCharGet(UART0_BASE);
+    UARTCharPut(UART0_BASE, aux_char);
+    if(aux_char == 'p')
+    uart_recebida =1;
+    if(aux_char == 's')
+    uart_recebida =0;
+
 }
 
 void ADC0handler(void)
@@ -96,18 +132,16 @@ void ADC0handler(void)
     // Passo 10
    // ( modulo | seq | Endereco de onde armazenar)
    ADCSequenceDataGet(ADC0_BASE, 0, ui32ADC0Value);
-   verifica_temp();
+   //verifica_temp();
 }
-
 
 void Systickhandler(void)
 {
-
-    ADCIntDisable(ADC0_BASE, 0);
+   ADCIntDisable(ADC0_BASE, 0);
    if(++ctd == 10)
    {
-    ctd=0;
-    IntEnable(INT_ADC0SS0);
-    ADCIntEnable(ADC0_BASE, 0);
+       ctd=0;
+       IntEnable(INT_ADC0SS0);
+       ADCIntEnable(ADC0_BASE, 0);
    }
 }
